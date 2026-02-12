6a17dfdbf25bdd4559546121d25785e54f4c7a70
Tomáš Bouška <tomas@buvis.net>
2026-02-12 18:48:22 +0100

    refactor(zettel): replace broad module import with explicit class imports in ZettelFactory

--- src/lib/buvis/pybase/zettel/domain/services/zettel_factory.py
+++ src/lib/buvis/pybase/zettel/domain/services/zettel_factory.py
@@ -1,18 +1,9 @@
-"""
-This module defines the ZettelFactory class which is responsible for creating instances of
-:class:`zettel_entities.Zettel`.
-
-Imports:
-    - :class:`zettel.domain.entities` for accessing Zettel entities.
-    - :class:`buvis.pybase.formatting.StringOperator` for string operations.
-"""
-
 from __future__ import annotations
 
-from typing import cast
-
-import buvis.pybase.zettel.domain.entities as zettel_entities
+from buvis.pybase.zettel.domain.entities import ProjectZettel, Zettel
 from buvis.pybase.formatting import StringOperator
+
+_ENTITY_CLASSES: dict[str, type[Zettel]] = {"ProjectZettel": ProjectZettel}
 
 
 class ZettelFactory:
@@ -23,7 +14,7 @@ class ZettelFactory:
     """
 
     @staticmethod
-    def create(zettel: zettel_entities.Zettel) -> zettel_entities.Zettel:
+    def create(zettel: Zettel) -> Zettel:
         """
         Create a Zettel instance, potentially downcasting it to a more specific type based on its 'type' attribute.
 
@@ -40,17 +31,13 @@ class ZettelFactory:
         # Try downcasting to more specific Zettel type
         class_name = StringOperator.camelize(zettel_type) + "Zettel"
 
-        try:
-            entity_class = cast(
-                type[zettel_entities.Zettel],
-                getattr(zettel_entities, class_name),
-            )
-        except AttributeError:
+        entity_class = _ENTITY_CLASSES.get(class_name)
+        if entity_class is None:
             return zettel
+
+        if zettel.from_rust:
+            downcasted_zettel = entity_class(zettel.get_data(), from_rust=True)
         else:
-            if zettel.from_rust:
-                downcasted_zettel = entity_class(zettel.get_data(), from_rust=True)
-            else:
-                downcasted_zettel = entity_class()
-                downcasted_zettel.replace_data(zettel.get_data())
-            return downcasted_zettel
+            downcasted_zettel = entity_class()
+            downcasted_zettel.replace_data(zettel.get_data())
+        return downcasted_zettel
